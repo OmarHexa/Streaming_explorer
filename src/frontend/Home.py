06f82328dd831dca9978d19aa0c0e8f0008984b5
Md Mijanur Rahman <md.rahman@fau.de>
2023-12-24 21:41:35 +0100

    working front page added

---
+++ src/frontend/Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def app():
+    st.title("Home")
+    st.write("Welcome to the home page!")
+    st.write("This is the first page of the multi-page app.")
+    st.write("Click on the pages on the left-hand side to navigate!")
+
+
+if __name__ == "__main__":
+    app()
